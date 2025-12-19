0d5e3917c6537795478d8d29047aee592f4ff853
= <=>
2025-12-19 11:19:55 +0530

    Created an exception handling code for file reading

--- app_streamlit.py
+++ app_streamlit.py
@@ -6,11 +6,26 @@ import requests
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#Exception handling for missing files
+def load_resource(file_path):
+    if not os.path.exists(file_path):
+        st.error(f"File not found: {file_path}")
+        return None
+    if os.path.getsize(file_path) == 0:
+        st.error(f"File is empty (0 bytes): {file_path}")
+        return None
+    try:
+        return joblib.load(file_path)
+    except EOFError:
+        st.error(f"EOFError: {file_path} is corrupted or incomplete.")
+        return None
 # -----------------------------
 # Load model and scaler
 # -----------------------------
-model = joblib.load("src/fraud_model.pkl")
-scaler = joblib.load("src/scaler.pkl")
+model = load_resource("src/fraud_model.pkl")
+scaler = load_resource("src/scaler.pkl")
+if model is None or scaler is None:
+    st.stop()
 
 # -----------------------------
 # Streamlit Config
